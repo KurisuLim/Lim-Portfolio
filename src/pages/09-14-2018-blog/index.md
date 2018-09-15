98d382f9c65f44c265b0c1c1451bb88c6cc5019c
KurisuLim <kazuki1002003@yahoo.com>
2018-09-14 19:55:57 -0700

    maintenance

---
+++ src/pages/09-14-2018-blog/index.md
@@ -0,0 +1,5 @@
+---
+path: '/first-post'
+title: 'Second Blog Post'
+---
+Hello! This is our Second blog post.
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>My Name is {process.env.FULL_NAME}</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello there!</h1>
+    <p>Sorry but this site is currently in maintenance.</p>
+    <p>The site will be online shortly.</p>
+    {/* <p>My Name is {process.env.FULL_NAME}</p>
+    <Link to="/page-2/">Go to page 2</Link> */}
   </div>
 )
 
