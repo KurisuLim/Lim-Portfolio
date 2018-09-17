906998ce953a47ee0242cdb990fc9db5c1fb29ab
KurisuLim <kazuki1002003@yahoo.com>
2018-09-17 11:31:22 -0700

    change in contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,50 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Contact = ()=> (
-// class Contact extends React.Component {
-
-//     render() {
-//         return (
-            <container>
-            <h2>Contact Form</h2>
-            <form
-            name="contact-form"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            >
-                <input
-                name='name'
-                type='text'
-                placeholder='Your Name'
-                required
-                />
-                <br/>
-                <input
-                name='email'
-                type='email'
-                placeholder='name@name.com'
-                required
-                />
-                <br/>
-                <textarea
-                name='message'
-                type='text'
-                placeholder='Message'
-                required
-                />
-                <br/>
-                <div data-netlify-recaptcha></div>
-                <button
-                type='submit'>
-                    Send Message
-                </button>
-                </form>
-            </container>
-//         )
-//     }
-// }
-// export default Contact;
-)
-export default Contact
+//const Contact = ()=> (
+class Contact extends React.Component {
+    render() {
+        return (
+    <div>
+        <h2>Contact Form</h2>
+        <form
+        name="contact-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        netlify
+        >
+        <input
+        name='name'
+        type='text'
+        placeholder='Your Name'
+        required
+        />
+        <br/>
+        <input
+        name='email'
+        type='email'
+        placeholder='name@name.com'
+        required
+        />
+        <br/>
+        <textarea
+        name='message'
+        type='text'
+        placeholder='Message'
+        required
+        />
+        <br/>
+        <div data-netlify-recaptcha></div>
+        <button
+        type='submit'>
+            Send Message
+        </button>
+        </form>
+    </div>
+        )
+    }
+}
+export default Contact;
+// )
+// export default Contact
